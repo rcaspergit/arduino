c3a14ffec811eff89dc0e165beb0a06e5a68ba7a
Rudy Casper <rudycasper@gmail.com>
2022-05-11 20:47:41 -0400

    Arduino projects
    new arduino projects

--- libraries/ds3231FS/config.h
+++
@@ -1,8 +0,0 @@
-#ifndef __config_h_
-  #define __config_h_
-
-  // comment this out if you need unixtime support
-  // this will add around 500 bytes to your firmware (depend of the card)
-  //#define CONFIG_UNIXTIME
-
-#endif
